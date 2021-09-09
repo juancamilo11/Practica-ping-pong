9786fa6105d6f7ed2a8bf5afdcba3c8bc444b637
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-09 03:49:55 -0500

    refactoing final - fin del proyecto

--- js/controller/main.js
+++ js/controller/main.js
@@ -4,6 +4,7 @@ import { Board } from "../model/Board.js";
 
 import { BoardView } from "../view/BoardView.js";
 
+//Declaración de variables para el juego
 let board = new Board(500,400);
 let canvas = document.getElementById('canvas');
 let bar1 = new Bar(10,60,10,30,board);
@@ -11,7 +12,7 @@ let bar2 = new Bar(480,60,10,30,board);
 let boardView = new BoardView(canvas,board);
 let ball = new Ball(250,70,6,board);
 
-
+//Eventos de captura de tecla 
 document.addEventListener('keydown',function(e){
    if(e.key == 'w'){
       bar1.up();
@@ -33,12 +34,16 @@ document.addEventListener('keydown',function(e){
    }
 });
 
+//Evento de reinicio de juego
 let botonReiniciar = document.getElementById('btn-reiniciar');
 botonReiniciar.addEventListener('click',() => {
    location.reload();
 })
 
+
+//Dibujar por primera vez el canvas
 boardView.draw();
+
 
 window.requestAnimationFrame(controller);
 
@@ -47,9 +52,15 @@ function controller() {
    window.requestAnimationFrame(controller);
 }
 
-let botonIniciar = document.getElementById('btn-jugar');
-botonIniciar.addEventListener('click',function iniciarJuego(e) {
-   let modal = document.querySelector('.modal-container');
-   modal.classList.add('hide');
-})
+//Funcion anónima autoejecutable para retirar la ventana de instrucciones
+(function(){
+   let botonIniciar = document.getElementById('btn-jugar');
+   
+   botonIniciar.addEventListener('click',function iniciarJuego(e) {
+      let modal = document.querySelector('.modal-container');
+      modal.classList.add('hide');
+   });
+
+})();
+
 
--- js/model/Ball.js
+++ js/model/Ball.js
@@ -1,76 +1,78 @@
 export class Ball {
-    constructor(x, y, radius, board) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.speed_y = 0;
-        this.speed_x = 3;
-        this.board = board;
-        this.direction = 1;
-        this.bounce_angle = 0;
-        this.max_bounce_angle = Math.PI / 12;
-        this.speed = 5;
+  constructor(x, y, radius, board) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.speed_y = 0;
+    this.speed_x = 3;
+    this.board = board;
+    this.direction = 1;
+    this.bounce_angle = 0;
+    this.max_bounce_angle = Math.PI / 12;
+    this.speed = 5;
 
-        board.ball = this;
-        this.kind = "circle";
-    }
-    get width() {
-        return this.radius * 2;
-    }
-    get height() {
-        return this.radius * 2;
-    }
-    move() {
-        this.x += this.speed_x * this.direction;
-        this.y += this.speed_y;
-    }
-    collisions(bar) {
-        let relative_intersect_y = bar.y + bar.height / 2 - this.y;
+    board.ball = this;
+    this.kind = "circle";
+  }
+  get width() {
+    return this.radius * 2;
+  }
+  get height() {
+    return this.radius * 2;
+  }
+  move() {
+    this.x += this.speed_x * this.direction;
+    this.y += this.speed_y;
+  }
+  collisions(bar) {
+    let relative_intersect_y = bar.y + bar.height / 2 - this.y;
 
-        let normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+    let normalized_intersect_y = relative_intersect_y / (bar.height / 2);
 
-        this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+    this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
 
-        this.speed_y = this.speed * -Math.sin(this.bounce_angle);
-        this.speed_x = this.speed * Math.cos(this.bounce_angle);
+    this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+    this.speed_x = this.speed * Math.cos(this.bounce_angle);
 
-        if (this.x > this.board.width / 2) this.direction = -1;
-        else this.direction = 1;
+    if (this.x > this.board.width / 2) this.direction = -1;
+    else this.direction = 1;
+  }
+
+  collision2() {
+    if (
+      250 + this.y + this.speed_y > this.board.height - this.radius ||
+      this.y + this.speed_y < this.radius
+    ) {
+      this.speed_y = -this.speed_y;
     }
+    this.collision3();
+  }
 
-    collision2() {
-        if((250 + this.y + this.speed_y > this.board.height - this.radius) ||
-            (this.y + this.speed_y < this.radius)) {
-            this.speed_y = -this.speed_y;
-        }
-        this.collision3();
+  collision3() {
+    let puntuacionA = document.getElementById("score-A");
+    let puntuacionB = document.getElementById("score-B");
+    if (this.x <= 0) {
+      //B gana el turno
+      let nuevaPuntuacion = parseInt(puntuacionB.textContent) + 1;
+      puntuacionB.textContent = nuevaPuntuacion;
+      this.reUbicarPelota(1);
+    } else if (this.x >= 495) {
+      //A gana el turno
+      let nuevaPuntuacion = parseInt(puntuacionA.textContent) + 1;
+      puntuacionA.textContent = nuevaPuntuacion;
+      this.reUbicarPelota(2);
     }
+  }
 
-    collision3(){
-      let puntuacionA = document.getElementById('score-A');
-      let puntuacionB = document.getElementById('score-B');
-      if(this.x <= 0){ //B gana el turno
-        let nuevaPuntuacion = parseInt(puntuacionB.textContent) + 1;
-        puntuacionB.textContent = nuevaPuntuacion;
-        this.reUbicarPelota(1);
-      } else if(this.x >=495) { //A gana el turno
-        let nuevaPuntuacion = parseInt(puntuacionA.textContent) + 1;
-        puntuacionA.textContent = nuevaPuntuacion;
-        this.reUbicarPelota(2);
-      }
+  reUbicarPelota(nuevoTurno) {
+    this.x = 250;
+    this.y = 70;
+    if (nuevoTurno == 1) {
+      this.speed_x = 3;
+      this.speed_y = 0;
+    } else if (nuevoTurno == 2) {
+      this.speed_x = 3;
+      this.speed_y = 0;
     }
-
-    reUbicarPelota(nuevoTurno) {
-      this.x = 250;
-      this.y = 70;
-      if(nuevoTurno == 1){
-        this.speed_x = 3;
-        this.speed_y = 0;
-      } else if(nuevoTurno ==2) {
-        this.speed_x = 3;
-        this.speed_y = 0;
-      }
-      //resetear la velocidad con random
-    }
+  }
 }
-
